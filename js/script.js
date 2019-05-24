5b0a3beb9816b599597c178ae97831da6bd6f0b7
Stefano <stefanonoventa@gmail.com>
2019-05-24 12:11:42 +0400

    add toggle disabled functionality

--- js/script.js
+++ js/script.js
@@ -23,15 +23,15 @@ $(document).ready(function() {
 	}
 
 	//t-shirts
-	$("#design").on("change", function(event) {  
-    const $colors = $("#color option");
+	$("#design").on("change", function(event) {
+		const $colors = $("#color option");
 		$colors.hide();
-    //show color only when a design is selected (change event is triggered)
-    
+		//show color only when a design is selected (change event is triggered)
+
 		//hide 'heart js' colors option when 'js puns is selected
 		if ($(this).val() === "js puns") {
-      hideAll($colors);
-      $("#colors-js-puns").fadeIn(300);
+			hideAll($colors);
+			$("#colors-js-puns").fadeIn(300);
 			//deselect any previously selected (if any)
 			$("#color option:selected").removeAttr("selected");
 			//select the first color for the selected design
@@ -42,8 +42,8 @@ $(document).ready(function() {
 		}
 		//hide 'js puns' colors option when 'heart js is selected
 		else if ($(this).val() === "heart js") {
-      hideAll($colors);
-      $("#colors-js-puns").fadeIn(300);
+			hideAll($colors);
+			$("#colors-js-puns").fadeIn(300);
 			$("#color option:selected").removeAttr("selected");
 			//select the appropriate color when design is changed
 			$colors.eq(3).attr("selected", true);
@@ -53,11 +53,53 @@ $(document).ready(function() {
 		} else {
 			//NO THEME SELECTED
 			//remove selected elements to prevent to send information not visually selected by the user
-      $("#color option:selected").removeAttr("selected");
+			$("#color option:selected").removeAttr("selected");
 			$("#colors-js-puns").fadeOut(200);
 		}
-  });
-  
-  //activities
-  
+	});
+
+	//activities
+	$(".activities").on("change", 'input[type="checkbox"]', function(event) {
+		const $activities = $(".activities label input");
+
+		//contains a string
+		const $clickedText = $(this)
+			.parent()
+			.text()
+			.replace(/\s+/g, "");
+
+		//contains jQuery input element
+		const $clicked = $(this);
+
+		//contains a regex obj
+		const regexDate = /\w+\s*\d{1,2}(am|pm)-\d{1,2}(am|pm)/gi;
+
+		//match and 'extract' the regex patter and remove any space
+		//date is a string
+		let date = $clickedText.match(regexDate)[0].replace(/\s+/g, "");
+
+		$activities.each( function(i, item) {
+      
+      const $currentItemText = $(this)
+				.parent()
+				.text()
+        .replace(/\s+/g, "");
+
+			if (!$clicked.prop("checked")) {
+        $(item).prop("disabled", false);
+					$(item)
+						.parent()
+						.css("textDecoration", "none");
+			} else {
+				if ($currentItemText.includes(date) && !$(item).is($clicked)) {
+					$(item).prop("disabled", true);
+					$(item)
+						.parent()
+						.css("textDecoration", "line-through");
+				}
+			}
+		});
+	});
 });
+
+//obj instanceof jQuery
